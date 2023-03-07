791bdfc5de2cd21f4e579bdc7f399c171c3d7453
addas3 <addas3code@gmail.com>
2023-03-07 18:17:35 +0000

    script java for all functions and script restart and rule button

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ var randNum = Math.round(Math.random()*2)
 var pointsMe = 0
 var pointsComp = 0
 
-var choicesComp = choices[randNum]
+var choiceComp = choices[randNum]
 
 //Below will take care of the computer and my score and update it.
 function score(){
@@ -15,3 +15,58 @@ function score(){
     document.querySelector(".result-comp").innerHTML = pointsComp
 }
 
+function elementIcon(object){
+    if(object === 'rock'){
+        return '✊'
+    }
+    else if(object === 'paper'){
+        return '✋'
+    }
+    else{
+        return '✌️'
+    }
+
+}
+
+function restartComp(){
+    randNum = Math.round(Math.random()*2)
+    choiceComp = choices[randNum]
+}
+
+function game(choiceMe){
+    document.querySelector("#choice-me").innerHTML = elementIcon(choiceMe)
+    document.querySelector("#choice-comp").innerHTML = elementIcon(choiceComp)
+
+    if(choiceMe === 'paper' && choiceComp === 'rock' || choiceMe === 'rock' && choiceComp === 'scissor' || choiceMe === 'scissor' && choiceComp === 'paper'){
+        pointsMe++
+        score()
+        document.querySelector(".text-start").innerHTML = "You Win!"
+    }
+    
+    else if(choiceMe === choiceComp){
+        score()
+        document.querySelector(".text-start").innerHTML = "It's a tie."
+    }
+
+    else{
+        pointsComp++
+        score()
+        document.querySelector(".text-start").innerHTML = "Computer Wins!"
+    }
+
+    // restart
+    restartComp()
+
+}
+
+// below restart function
+function restart(){
+    pointsMe = 0
+    pointsComp = 0
+    score()
+}
+
+// below restart function
+function rules(){
+    alert("The Rules are simple for winning. The Scissors wins against The Paper, The Rock wins against The Scissors, and The Paper wins against The Rock.")
+}
