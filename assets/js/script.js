50012ea0a0a2e0ea032502293d9626d8c1ba03a7
addas3 <addas3code@gmail.com>
2023-04-28 17:14:15 +0000

    Add function getcoputerchoice and adjust names

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,15 @@
 // game variables
 var choices = ["rock", "scissors", "paper"];
-// here we get random number between 0-2
-var randNum = Math.round(Math.random()*2);
 
 
-var pointsMe = 0;
-var pointsComp = 0;
+var userScore = 0;
+var compScore = 0;
 
-var choiceComp = choices[randNum];
 
-//Below will take care of the computer and my score and update it.
-function score(){
-    document.querySelector(".result-user").innerHTML = pointsMe;
-    document.querySelector(".result-comp").innerHTML = pointsComp;
+//Below will take care of the computer and my updateScore and update it.
+function updateScore(){
+    document.querySelector(".result-user").innerHTML = userScore;
+    document.querySelector(".result-comp").innerHTML = compScore;
 }
 
 function elementIcon(object){
@@ -28,41 +25,34 @@ function elementIcon(object){
 
 }
 
-function restartComp(){
-    randNum = Math.round(Math.random()*2);
-    choiceComp = choices[randNum];
+function getComputerChoice(){
+    // here we get random number between 0-2
+    const randNum = Math.round(Math.random()*2);
+    return choices[randNum];
 }
 
 function game(choiceMe){
+    var choiceComp = getComputerChoice();
     document.querySelector("#choice-me").innerHTML = elementIcon(choiceMe);
     document.querySelector("#choice-comp").innerHTML = elementIcon(choiceComp);
 
-    if((choiceMe === 'paper' && choiceComp === 'rock') || (choiceMe === 'rock' && choiceComp === 'scissors') || (choiceMe === 'scissors' && choiceComp === 'paper')){
-        pointsMe++;
-        score();
+    if((choiceMe === 'paper' && choiceComp === 'rock') || (choiceMe === 'rock' && choiceComp === 'scissors') || (choiceMe === 'scissors' && choiceComp === 'paper')) {
+        userScore++;
+        updateScore();
         document.querySelector(".text-start").innerHTML = "You Win!";
-    }
-    
-    else if(choiceMe === choiceComp){
-        score();
+    } else if(choiceMe === choiceComp) {
+        updateScore();
         document.querySelector(".text-start").innerHTML = "It's a tie.";
-    }
-
-    else{
-        pointsComp++;
-        score();
+    } else {
+        compScore++;
+        updateScore();
         document.querySelector(".text-start").innerHTML = "Computer Wins!";
     }
-
-    // restart
-    restartComp();
-
 }
 
 // below restart function
 function restart(){
-    pointsMe = 0;
-    pointsComp = 0;
-    score();
+    userScore = 0;
+    compScore = 0;
+    updateScore();
 }
-
